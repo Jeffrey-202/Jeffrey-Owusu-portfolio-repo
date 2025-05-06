944adbe5381a9bd13726cffa2687cd67577f32cb
Jeffrey-02 <c00314408@setu.ie>
2025-05-06 20:14:32 +0100

    Move build files to root for GitHub Pages

---
+++ _app/immutable/chunks/BtiN_VYx.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_vck7zk)==null?void 0:s.base)??"/Jeffrey-Owusu-portfolio-repo";var e;const a=((e=globalThis.__sveltekit_vck7zk)==null?void 0:e.assets)??t;export{a,t as b};
